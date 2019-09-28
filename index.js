fcb791700cbe103555ae8ee34abf6b7b9b8011ab
ericHumanFriend <eric.q.kozak@gmail.com>
2019-09-27 21:40:58 -0700

    something broke on the last one, this should *actually* make Heroku work

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const PORT = process.env.PORT || 5000;
 const VIEWS = path.join(__dirname, 'views');
 
 express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', VIEWS)
-    .set('view engine', 'html')
-    .use(express.static('public'))
-    .use(express.static(path.join(__dirname, 'public')))
-    .get('/', (req, res) => res.sendFile('calculator.html', { root : VIEWS }))
-    .listen(PORT, () => consolelog(`Listening on ${ PORT }`));
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', VIEWS)
+  .set('view engine', 'html')
+  .use(express.static('public'))
+  .use(express.static(path.join(__dirname, 'public')))
+  .get('/', (req, res) => res.sendFile('calculator.html', { root : VIEWS }))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
