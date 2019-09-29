20672dfce0ac867750ebffb871e0273fdd820b44
ericHumanFriend <eric.q.kozak@gmail.com>
2019-09-29 02:38:59 -0700

    NO FUNCTIONALITY CHANGED - rewrote index.js so it no longer contains borrowed code

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-/*
-The contents of this file are copied directly from Nick Yang's work temporarily only in an attempt
-to find any way to get Heroku to work. It will be replaced with my original work as soon as possible.
-*/
-
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
+const app = express();
 
-const VIEWS = path.join(__dirname, 'views');
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', VIEWS)
-  .set('view engine', 'html')
-  .use(express.static('public'))
-  .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.sendFile('calculator.html', { root : VIEWS }))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req, res) {
+    res.sendFile('calculator.html', { root : path.join(__dirname, 'views') })
+});
+app.listen(8080, function() {
+    console.log(`Listening on 8080`)
+});
