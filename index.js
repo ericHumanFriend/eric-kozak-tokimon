572a29c8e916bbd7a03f8ca2d4a2b7ad80da93b0
ericHumanFriend <eric.q.kozak@gmail.com>
2019-10-15 21:26:33 -0700

    Can now perform database queries.

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 const PORT = process.env.PORT || 5000;
+
+const {Pool} = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: false
+});
+
+console.log(process.env.DATABASE_URL);
 
 const VIEWS = path.join(__dirname, 'views');
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', VIEWS)
-  .set('view engine', 'html')
-  .use(express.static('public'))
-  .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.sendFile('calculator.html', { root : VIEWS }))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', VIEWS)
+    .set('view engine', 'ejs')
+    .use(express.static('public'))
+    .use(express.static(path.join(__dirname, 'public')))
+    .get('/', (req, res) => {
+        var tokiQuery = `SELECT * FROM tokimon`;
+
+        pool.query(tokiQuery, (error, result) => {
+            if (error) {
+                console.log("oh man");
+                res.end(error.toString());
+            }
+            results = {'tokimon': result.rows};
+            res.render('calculator', results)
+        })
+    })
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
