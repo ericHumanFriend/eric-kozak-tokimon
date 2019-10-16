30e1a36034ba786af2c9740d4fb330a1e5d09f30
ericHumanFriend <eric.q.kozak@gmail.com>
2019-10-15 17:03:26 -0700

    Add postgres

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const path = require('path');
+const {Pool} = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+});
 const PORT = process.env.PORT || 5000;
 
 const VIEWS = path.join(__dirname, 'views');
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', VIEWS)
-  .set('view engine', 'html')
-  .use(express.static('public'))
-  .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.sendFile('tokimon.html', { root : VIEWS }))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', VIEWS)
+    .set('view engine', 'html')
+    .use(express.static('public'))
+    .use(express.static(path.join(__dirname, 'public')))
+    .get('/', (req, res) => res.sendFile('index.html', {root: VIEWS}))
+    .get('/db', async (req, res) => {
+        try {
+            const client = await pool.connect()
+            const result = await client.query('SELECT * FROM test_table');
+            const results = {'results': (result) ? result.rows : null};
+            res.render('pages/db', results);
+            client.release();
+        } catch (err) {
+            console.error(err);
+            res.send("Error " + err);
+        }
+    })
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
