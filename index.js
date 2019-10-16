1139d96a398dca98953be1cae670f0cc32b1ea19
ericHumanFriend <eric.q.kozak@gmail.com>
2019-10-15 19:06:58 -0700

    back to the calculator again

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
 const express = require('express');
 const path = require('path');
-const {Pool} = require('pg');
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-});
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 const VIEWS = path.join(__dirname, 'views');
 
-console.log("DATABASE_URL:")
-console.log(process.env.DATABASE_URL);
-
 express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', VIEWS)
-    .set('view engine', 'html')
-    .use(express.static('public'))
-    .use(express.static(path.join(__dirname, 'public')))
-    .get('/', (req, res) => {
-
-        let query = `SELECT * FROM tokimon`;
-
-        pool.query(query, (error, result) => {
-            console.log("oh no");
-            if (error) {
-                res.end(error);
-            }
-            let results = {'tokimon': result.rows};
-            res.render('views/menu', results)
-        });
-    })
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', VIEWS)
+  .set('view engine', 'html')
+  .use(express.static('public'))
+  .use(express.static(path.join(__dirname, 'public')))
+  .get('/', (req, res) => res.sendFile('calculator.html', { root : VIEWS }))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
---
+++ public/js/calculator.js
@@ -0,0 +1,112 @@
+let calculator = document.getElementById("calculator");
+
+let numericPercentages = [];
+
+function getPercentages() {
+    let numerators = calculator.getElementsByClassName("numerator");
+    let denominators = calculator.getElementsByClassName("denominator");
+    let percentages = calculator.getElementsByClassName("percent");
+    let numRows = calculator.rows.length - 1;
+
+    numericPercentages.length = 0;
+
+    for(let i = 0; i < numRows; i++) {
+        let numerator = numerators[i].value;
+        let denominator = denominators[i].value;
+
+        let grade = "";
+
+        if(numerator.length !== 0 && denominator.length !== 0) {
+            numerator = parseFloat(numerator);
+            denominator = parseFloat(denominator);
+
+            if(isNaN(numerator) || isNaN(denominator)) {
+                grade = "Invalid input";
+            } else if(denominator <= 0) {
+                grade = "Denominator must be positive and nonzero";
+            } else {
+                grade = 100 * numerator / denominator;
+                numericPercentages.push(grade);
+                grade = grade.toFixed(2) + "%";
+            }
+        }
+
+        percentages[i].innerText = grade;
+    }
+}
+
+function getMean() {
+    let numRows = calculator.rows.length - 1;
+
+    let average = "Invalid percentages to calculate mean";
+    if(numericPercentages.length === numRows) {
+        let total = numericPercentages.reduce(function(total, element) {
+            return total + element;
+        }, 0);
+
+        average = total / numRows;
+        average = average.toFixed(2) + "%";
+    }
+
+    document.getElementById("result").innerText = average;
+}
+
+function getWeighted() {
+    let weights = calculator.getElementsByClassName("weight");
+    let numRows = calculator.rows.length - 1;
+
+    let average = "Invalid weights or percentages to calculate mean";
+    if(numericPercentages.length === numRows) {
+        let valid = true;
+        let total = 0;
+        let totalWeights = 0;
+
+        for(let i = 0; i < numRows; i++) {
+            let weight = parseFloat(weights[i].value);
+
+            if(isNaN(weight)) {
+                valid = false;
+                break;
+            } else if(weight < 0) {
+                average = "Weights cannot be negative";
+            } else {
+                total += numericPercentages[i] * weight;
+                totalWeights += weight;
+            }
+        }
+
+        if(valid) {
+            average = total / totalWeights;
+            average = average.toFixed(2) + "%";
+        }
+
+    }
+
+    document.getElementById("result").innerText = average;
+}
+
+function addRow() {
+    let row = calculator.rows[1];
+    let clone = row.cloneNode(true);
+    let newRow = calculator.appendChild(clone);
+
+    let activityName = newRow.getElementsByClassName("activity_name")[0];
+    activityName.innerText = "Activity " + (calculator.rows.length - 1);
+    let shortName = newRow.getElementsByClassName("short_name")[0];
+    shortName.innerText = "A" + (calculator.rows.length - 1);
+
+    let inputs = newRow.getElementsByTagName("input");
+    for(let i = 0; i < inputs.length; i++) {
+        inputs[i].value = "";
+    }
+
+    getPercentages();
+}
+
+function removeRow() {
+    if(calculator.rows.length > 2) {
+        calculator.deleteRow(calculator.rows.length - 1);
+    }
+
+    getPercentages();
+}
