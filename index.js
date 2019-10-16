157272740b499dc91f71fc2882448b580381a5db
ericHumanFriend <eric.q.kozak@gmail.com>
2019-10-16 00:29:42 -0700

    Auto reformatted code.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ express()
     .set('views', VIEWS)
     .set('view engine', 'ejs')
     .use(express.json())
-    .use(express.urlencoded({ extended: false }))
+    .use(express.urlencoded({extended: false}))
     .use(express.static('public'))
     .use(express.static(path.join(__dirname, 'public')))
     .get('/', (req, res) => {
